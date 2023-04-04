da6fab548f234eb211c24ef3b7d85c82b220e210
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-04-05 00:58:31 +0200

    commit

--- code/chapter/game.js
+++ code/chapter/game.js
@@ -12,7 +12,7 @@ let simpleLevelPlan = `
 //The following class stores a level object. Its argument should be the string that defines the level.
 class Level {
     constructor(plan) {
-        let rows = pln.plan.trim().split("\n").map(l => [...l]);
+        let rows = plan.trim().split("\n").map(l => [...l]);
         this.height = rows.length;
         this.width = rows[0].length;
         this.startActors = [];
@@ -22,7 +22,7 @@ class Level {
                 let type = levelChars[ch];
                 if (typeof type == "string") return type;
                 this.startActors.push(
-                    type.create(new VideoColorSpace(x, y), ch)
+                    type.create(new Vec(x, y), ch)
                 );
                 return "empty";
             });
@@ -96,3 +96,202 @@ class Lava {
     }
 }
 Lava.prototype.size = new Vec(1, 1);
+
+//A coin object stores a base position as well as a wobble property that tracks the phase of the bouncing motion. 
+class Coin {
+    constructor(pos, basePos, wobble) {
+        this.pos = pos;
+        this.basePos = basePos;
+        this.wobble = wobble;
+    }
+
+    get type() {
+        return "coin";
+    }
+
+    static create(pos) {
+        let basePos = pos.plus(new Vec(0.2, 0.1));
+        return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+    }
+}
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+//the levelChars object that maps plan characters to either background grid types or actor classes.
+const levelChars = {
+    ".": "empty",
+    "#": "wall",
+    "+": "lava",
+    "@": Player,
+    "o": Coin,
+    "=": Lava,
+    "|": Lava,
+    "v": Lava
+};
+//That gives us all the parts needed to create a Level instance.
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+
+//The display type we define in this chapter is called DOMDisplay because it uses DOM elements to show the level.
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for (let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]);
+    }
+    for (let child of children) {
+        dom.appendChild(child);
+    }
+    return dom;
+}
+
+//A display is created by giving it a parent element to which it should append itself and a level object.
+class DOMDisplay {
+    constructor(parent, level) {
+        this.dom = elt("div", {
+            class: "game"
+        }, drawGrid(level));
+        this.actorLayer = null;
+        parent.appendChild(this.dom);
+    }
+    clear() {
+        this.dom.remove();
+    }
+}
+
+//The scale constant gives the number of pixels that a single unit takes up on the screen.
+const scale = 20;
+
+function drawGrid(level) {
+    return elt("table", {
+        class: "background",
+        style: `width: ${level.width * scale}px`
+    }, ...level.rows.map(row =>
+        elt("tr", {
+                style: `height: ${scale}px`
+            },
+            ...row.map(type => elt("td", {
+                class: type
+            })))));
+}
+
+function drawActors(actors) {
+    return elt("div", {}, ...actors.map(actor => {
+        let rect = elt("div", {
+            class: `actor ${actor.type}`
+        });
+        rect.syle.width = `${actor.size.x}px`;
+        rect.style.height = `${actor.size.y}px`;
+        rect.style.left = `${actor.pos.x * scale}px`;
+        rect.style.top = `${actor.pos.y * scale}px`;
+        return rect;
+    }));
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+    if (this.actorLayer) this.actorLayer.remove();
+    this.actorLayer = drawActors(state.actors);
+    this.dom.appendChild(this.actorLayer);
+    this.dom.className = `game ${state.status}`;
+    this.scrollPlayerIntoView(state);
+};
+
+//In the scrollPlayerIntoView method, we find the player’s position and update the wrapping element’s scroll position. 
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+    let width = this.dom.clientWidth;
+    let height = this.dom.clientHeight;
+    let margin = width / 3;
+
+    //The viewport
+    let left = this.dom.scrollLeft,
+        right = left + width;
+    let top = this.dom.scrollTop,
+        bottom = top + height;
+
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5))
+        .times(scale);
+
+    if (center.x < left + margin) {
+        this.dom.scrollLeft = center.x - margin;
+    } else if (center.x > right - margin) {
+        this.dom.scrollLeft = center.x + margin - width
+    }
+    if (center.y < top + margin) {
+        this.dom.scrollTop = center.y - margin;
+    } else if (center.y > bottom - margin) {
+        this.dom.scrollTop = center.y + margin - height;
+    }
+}
+
+//Motion and Collision
+//This method tells us whether a rectangle (specified by a position and a size) touches a grid element of the given type.
+Level.prototype.touches = function (pos, size, type) {
+    let xStart = Math.floor(pos.x);
+    let xEnd = Math.ceil(pos.x + size.x);
+    let yStart = Math.floor(pos.y);
+    let yEnd = Math.floor(pos.y + size.y);
+
+    for (let y = yStart; y < yEnd; y++) {
+        for (let x = xStart; x < xEnd; x++) {
+            let isOutside = x < 0 || x >= this.width ||
+                y < 0 || y >= this.height;
+            let here = isOutside ? "wall" : this.rows[y][x];
+            if (here == type) return true;
+        }
+    }
+    return false;
+};
+
+//The state update method uses touches to figure out whether the player is touching lava.
+State.prototype.update = function (time, keys) {
+    let actors = this.actors
+        .map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+    if (newState.status != "playing") return newState;
+
+    let player = newState.player;
+    if (this.level.tou(player.pos, player.size, "lava")) {
+        return new State(this.level, actors, "lost");
+    }
+
+    for (let actor of actors) {
+        if (actor != player && overlap(actor, player)) {
+            newState = actor.collide(newState);
+        }
+    }
+    return newState;
+}
+
+/*Overlap between actors is detected with the overlap function. It takes
+two actor objects and returns true when they touch—which is the case when they 
+overlap both along the x-axis and along the y-axis.*/
+function overlap(actor1, actor2) {
+    return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+        actor1.pos.x < actor2.pos.x + actor2.size.x &&
+        actor1.pos.y + actor1.size.y > actor2.pos.y &&
+        actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+/*If any actor does overlap, its collide method gets a chance to update the state.
+Touching a lava actor sets the game status to "lost". Coins vanish when you touch 
+them and set the status to "won" when they are the last coin of the level.*/
+Lava.prototype.collide = function (state) {
+    return new State(state.level, state.actors, "lost");
+}
+Coin.prototype.collide = function (state) {
+    let filtered = state.actors.filtered(a => a != this);
+    let status = state.status;
+    if (!filtered.some(a.type == "coin")) status = "won";
+    return new State(state.level, filtered, status);
+}
+
+//This update method computes a new position by adding the product of the time step and the current speed to its old position. 
+Lava.prototype.update = function (time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+    if (!state.level.touches(newPos, this.size, "wall")) {
+        return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+        return new Lava(this.reset, this.speed, this.reset);
+    } else {
+        return new Lava(this.pos, this.speed.times(-1));
+    }
+};
